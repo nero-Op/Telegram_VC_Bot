5cb5915c855ea1c8f39ba59679591eb9deecefad
DRAGON SAMA <51018664+MrBhavya@users.noreply.github.com>
2021-02-06 06:37:21 +0000

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1563090948:AAFR5zoKHGAXeX29nBu4o00DH8GCG2h4pd8"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001340205063
+owner_id = 1154348892
